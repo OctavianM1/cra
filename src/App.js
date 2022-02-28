a0acc16db3fb720de2c5de1af64feec48491c071
Octavian Mitu <octavian.mitu@amdaris.com>
2022-02-28 08:59:16 +0200

    added feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React {process.env.REACT_APP_TEST}
+          Learn feature React {process.env.REACT_APP_TEST}
         </a>
       </header>
     </div>
