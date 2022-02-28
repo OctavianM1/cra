7df4856fa503af147be19f2ca8370167a1e6fcac
Octavian Mitu <octavian.mitu@amdaris.com>
2022-02-28 08:02:27 +0200

    env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {process.env.REACT_APP_TEST}
         </a>
       </header>
     </div>
